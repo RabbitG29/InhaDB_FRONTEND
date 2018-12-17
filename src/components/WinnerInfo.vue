27aadeba1aa74fbc4a3e2b6c590e728f4a17d618
LYJ <yynn0408@gmail.com>
2018-12-18 03:49:20 +0900

    당선자

--- src/components/WinnerInfo.vue
+++ src/components/WinnerInfo.vue
@@ -3,19 +3,162 @@
     <h1>Winner Info</h1>
     <router-link :to="{ name: 'HelloWorld', params: {} }">홈으로</router-link>
     <br>
-    <h2>당선자 정보들.....</h2>
+    <h3>정후보</h3>
+    <table class="table table-striped">
+    <thead>
+       <tr class="text-center">
+        <th class="text-center" scope="col">이력번호</th>
+        <th class="text-center">년도</th>
+        <th class="text-center">이력내용</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in seclist" @click="readBoard(item)" :key="index" style="cursor: pointer">
+        <td>{{item.이력번호}}</td>
+        <td>{{item.연도}}</td>
+        <td>{{item.이력내용}}</td>
+      </tr>
+    </tbody>
+    </table>
+    <h3>부후보</h3>
+    <table class="table table-striped">
+    <thead>
+       <tr class="text-center">
+        <th class="text-center" scope="col">이력번호</th>
+        <th class="text-center">년도</th>
+        <th class="text-center">이력내용</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in seclist2" @click="readBoard(item)" :key="index" style="cursor: pointer">
+        <td>{{item.이력번호}}</td>
+        <td>{{item.연도}}</td>
+        <td>{{item.이력내용}}</td>
+      </tr>
+
+    </tbody>
+    </table>
     <br>
     <h2>실시 공약들...</h2>
+    <table class="table table-striped">
+      <thead>
+         <tr class="text-center">
+          <th class="text-center" scope="col">공약번호</th>
+          <th class="text-center">공약내용</th>
+          <th class="text-center">이행여부</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in list" @click="readBoard(item)" :key="index" style="cursor: pointer">
+          <td>{{item.공약번호}}</td>
+          <td>{{item.공약내용}}</td>
+          <td>{{item.이행여부}}</td>
+        </tr>
+      </tbody>
+    </table>
     <br>
     <h2>실시 사업들...</h2>
+    <table class="table table-striped">
+      <thead>
+         <tr class="text-center">
+          <th class="text-center" scope="col">공약 외 사업번호</th>
+          <th class="text-center">사업 이름</th>
+          <th class="text-center">사업내용</th>
+          <th class="text-center">사업평가</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in thlist" @click="readBoard(item)" :key="index" style="cursor: pointer">
+          <td>{{item.사업번호}}</td>
+          <td>{{item.사업이름}}</td>
+          <td>{{item.사업내용}}</td>
+          <td>{{item.사업평가}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="row">
+        <!-- print meeting log list -->
+
+      </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'WinnerInfo'
+    name : "WinnerInfo",
+    data() {
+      return {
+        msg: '선거정보',
+        list: [],
+        seclist:[],
+        sectlist2:[],
+        thlist:[],
+        winid: ''
+      }
+    },
+
+    methods: {
+      getData: function() {
+        var url = this.$config.targetURL+'/vote/winnerInfo/in/'+this.winid;
+        console.log(url)
+        this.$http.get(url)
+        .then(result=> {
+          console.log(result)
+          this.list = JSON.parse(result.data.result)
+    //      console.log("?")
+          console.log(this.list)
+      //    console.log("!")
+        })
+        .catch(error=> {
+          console.log('서버에러')
+        })
+      },
+      getData2: function() {
+        var url2 = this.$config.targetURL+'/vote/winnerInfo/in/career/'+this.winid;
+        this.$http.get(url2)
+        .then(result=> {
+          console.log(result)
+          this.seclist = JSON.parse(result.data.result)
+          this.seclist2=JSON.parse(result.data.result2)
+      //    console.log("??")
+          console.log(this.seclist)
+          console.log(this.seclist2)
+        //  console.log("!!")
+        })
+        .catch(error=> {
+          console.log('서버에러')
+        })
+      },
+    getData3: function() {
+      var url3 = this.$config.targetURL+'/vote/winnerInfo/in/busi/'+this.winid;
+      console.log(url3)
+      this.$http.get(url3)
+      .then(result=> {
+        console.log(result)
+        console.log("?????????")
+        this.thlist = JSON.parse(result.data.result)
+        console.log("???")
+        console.log(this.thlist)
+        console.log("!!!")
+      })
+      .catch(error=> {
+        console.log('서버에러')
+      })
+    }
+  },
+    mounted: function(){
+      this.msg = ''
+      this.winid = this.$route.params.winid
+      console.log('회차 : '+this.winid)
+      this.getData()
+      this.getData2()
+      this.getData3()
+    }
 }
+
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- src/components/WinnerList.vue
+++ src/components/WinnerList.vue
@@ -3,25 +3,178 @@
     <h1>Winner</h1>
     <router-link :to="{ name: 'HelloWorld', params: {} }">홈으로</router-link>
     <br>
-    <router-link :to="{ name: 'WinnerInfo', params: {} }">1회 당선자</router-link>
+    <div type="text/x-template" v-for="(item, index) in list" :key="index" id="blog-card">
+    <router-link :to="{ name: 'WinnerInfo', params: {winid: item.선거회차 } }"> {{item.선거회차}}회 당선인</router-link>
+      <a class="card-link" href="#">
+        <article class="blog-card">
+          <img class="post-image" :src="image" />
+          <div class="article-details">
+            <h3 class="post-title">{{ item.기호 }}</h3>
+            <p class="post-description">{{ item.정후보 }}</p>
+            <p class="post-description">{{ item.정후보이름 }}</p>
+            <p class="post-description">{{ item.부후보 }}</p>
+            <p class="post-description">{{ item.부후보이름 }}</p>
+          </div>
+
+
+        </article>
+      </a>
     <br>
-    <router-link :to="{ name: 'WinnerInfo', params: {} }">2회 당선자</router-link>
+  </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'WinnerList'
+  name: 'WinnerList',
+  data() {
+    return {
+      msg: '당선자정보',
+      list: [],
+      image: 'https://source.unsplash.com/DnWYw0zLJBg',
+      winid:''
+    }
+  },
+  mounted: function() {
+    console.log("mounted")
+    this.getData()
+    console.log("ㅠㅠ")
+  },
+  methods: {
+    getData: function() {
+      var url = this.$config.targetURL+'/vote/winnerInfo';
+      console.log(url)
+      this.$http.get(url)
+      .then(result=> {
+        console.log(result)
+        this.list = JSON.parse(result.data.result)
+        console.log("?")
+        console.log(this.list)
+        console.log("!")
+      })
+      .catch(error=> {
+        console.log('서버에러')
+      })
+    }
+   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.rank {
-  background-color: rgb(240,240,240);
-  font-size: 1.1em;
+
+
+body {
+  display: flex;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  color: $text;
+  background: $bg;
+  font-size: 0.9375rem;
+  min-height: 100vh;
+  margin: 0;
+  line-height: 1.6;
+  align-items: center;
+  justify-content: center;
+  text-rendering: optimizeLegibility;
 }
-h3 {
-  margin: 20px;
+
+#container {
+  width: 30rem;
+  height: 13.625rem;
+}
+
+.blog-card {
+  display: flex;
+  flex-direction: row;
+  background: $white;
+  box-shadow: 0 0.1875rem 1.5rem $shadow;
+  border-radius: 0.375rem;
+  overflow: hidden;
+}
+
+
+.post-image {
+  @include transition(opacity 0.3s ease);
+  display: block;
+  width: 100%;
+   object-fit: cover;
+}
+
+.article-details {
+  padding: 1.5rem;
+}
+
+.post-category {
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  font-weight: 700;
+  line-height: 1;
+  letter-spacing: 0.0625rem;
+  margin: 0 0 0.75rem 0;
+  padding: 0 0 0.25rem 0;
+  border-bottom: 0.125rem solid $border;
+}
+
+.post-title {
+  @include transition(color 0.3s ease);
+  font-size: 1.125rem;
+  line-height: 1.4;
+  color: $black;
+  font-weight: 700;
+  margin: 0 0 0.5rem 0;
+}
+
+.post-author {
+  font-size: 0.875rem;
+  line-height: 1;
+  margin: 1.125rem 0 0 0;
+  padding: 1.125rem 0 0 0;
+  border-top: 0.0625rem solid $border;
+}
+
+@media (max-width: 40rem) {
+  #container {
+    width: 18rem;
+    height: 27.25rem;
+  }
+
+  .blog-card {
+    flex-wrap: wrap;
+  }
+}
+
+@supports (display: grid) {
+  body {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 0.625rem;
+    grid-template-areas: ". main main ." ". main main .";
+  }
+
+  #container {
+    grid-area: main;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .post-image {
+    height: 100%;
+  }
+
+  .blog-card {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
+  }
+
+  @media (max-width: 40rem) {
+    .blog-card {
+      grid-template-columns: auto;
+      grid-template-rows: 12rem 1fr;
+    }
+  }
 }
 </style>
