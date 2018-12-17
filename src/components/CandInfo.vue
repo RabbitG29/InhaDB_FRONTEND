7c71a6f35dad9670306e3632aed4afe0d6465f96
changsubeen <chickby@hanmail.net>
2018-12-17 19:29:39 +0900

    후보자 정보 뒤로가기 버튼2

--- src/components/CandInfo.vue
+++ src/components/CandInfo.vue
@@ -2,8 +2,14 @@
   <div>
   <div class="container">
     <h1>후보자정보</h1>
+    <a href="#" class="btn btn-secondary" style="float:right cursor: pointer" @click="goBack">뒤로가기</a>
     <div>
       <div id="card-candidate">
+        <div class="row form-group">
+          <div class="col-sm-10"></div>
+          <div class="col-sm-2" >
+          </div>
+        </div>
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">기호 {{list[0].기호}}번</h5>
